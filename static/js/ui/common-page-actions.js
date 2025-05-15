2542d76557802ef9319d860fd46b73842423f5d9
Maksim Mikhailov <87376297+CodeAnge1@users.noreply.github.com>
2025-05-15 20:11:52 +0300

    Added theme saving feature

--- static/js/ui/common-page-actions.js
+++ static/js/ui/common-page-actions.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeSwitcher = document.getElementById('darkmode-toggle');
 
-    themeSwitcher.addEventListener('click', () => {
-        themeSwitcher.classList.toggle('light');
-        themeSwitcher.classList.toggle('dark');
-        document.body.classList.toggle('dark-theme');
-        Array.from(document.getElementsByClassName('svg-image')).forEach((elem) => {
-            elem.classList.toggle('svg-white');
-        });
-    });
+    if (localStorage.getItem('dark-theme') === 'true') {
+        themeSwitcher.checked = true;
+        changeTheme();
+    }
+
+    themeSwitcher.addEventListener('click', changeTheme);
 });
+
+function changeTheme() {
+    const themeSwitcher = document.getElementById('darkmode-toggle');
+
+    themeSwitcher.classList.toggle('light');
+    themeSwitcher.classList.toggle('dark');
+    document.body.classList.toggle('dark-theme');
+    Array.from(document.getElementsByClassName('svg-image')).forEach((elem) => {
+        elem.classList.toggle('svg-white');
+    });
+    localStorage.setItem('dark-theme', String(document.body.classList.contains('dark-theme')))
+}
